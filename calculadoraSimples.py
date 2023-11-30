33ab0fb75c6d45c3cc543e3f906f0be758ab7131
Ruan Sampaio <142833359+importantduck@users.noreply.github.com>
2023-11-30 20:12:54 +0000

    first

---
+++ calculadoraSimples.py
@@ -0,0 +1,9 @@
+n1 = input ("Digite o primeiro número:") # A função input atribui o valor a variável n1 em forma de string
+n2 = input ("Digite o segundo número:")
+
+print (int(n1)+int(n2)) # Transforma as variáveis em int e depois soma
+
+n1 = int(input ("Digite o primeiro número:")) # Transforma o valor recebido pelo input em int
+n2 = int(input ("Digite o segundo número:"))
+
+print (n1+n2) 
