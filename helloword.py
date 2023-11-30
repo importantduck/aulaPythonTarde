d7fef8a268d2c5105d17d099b35effdb09b7e1f6
Ruan Sampaio <142833359+importantduck@users.noreply.github.com>
2023-11-30 15:17:20 -0300

    Create helloword.py
    first

---
+++ helloword.py
@@ -0,0 +1,9 @@
+print("Hello Word !") # Mostra na tela o texto Hello Word!
+mensagem = "Olá, tudo bem?"
+print(mensagem) # Mostra a mensagem na tela
+print(mensagem[1]) # Mostra a segunda letra da mensagem
+print(mensagem[0:4]) # Mostra da primeira (0) até a quarta (4) da mensagem
+print(mensagem[-1]) # Mostra a última (-1) letra da mensagem
+print(mensagem[4::]) # Mostra da quarta (4) posição até o fim
+print(mensagem[10:13]) # Mostra a palavra "bem" da mensagem
+
